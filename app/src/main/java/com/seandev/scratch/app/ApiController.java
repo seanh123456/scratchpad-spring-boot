05dcf47cc8482c54854e817a90e35b0c45171ac5
Sean Hutchinson <sean.hutchinson93@gmail.com>
2020-11-01 01:11:23 -0500

    Add login, logout not working

--- app/src/main/java/com/seandev/scratch/app/ApiController.java
+++ app/src/main/java/com/seandev/scratch/app/ApiController.java
@@ -5,24 +5,29 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
+@CrossOrigin(origins={ "http://localhost:3000", "http://localhost:8080" })
 public class ApiController {
 
 	@Autowired
 	private CustomerRepository repository;
-	
-	@GetMapping("/api")
+
+	@GetMapping("/")
 	public Map<String, String> homepage() {
 		Map<String, String> message = new HashMap<>();
 		Customer customer = repository.findByFirstName("Alice");
 		message.put("message", String.format("Hello %s %s from Spring Boot World!", customer.firstName, customer.lastName));
+		System.out.println("hey: " + customer.toString());
 		return message;
 	}
 	
-	@GetMapping("/api/counter/increment")
+	@GetMapping("/counter/increment")
 	public Customer increment(String id) {
 		Optional<Customer> customer = repository.findById(id);
 		if (customer.isPresent()) {
@@ -34,7 +39,7 @@ public class ApiController {
 		return null;
 	}
 	
-	@GetMapping("/api/counter/decrement")
+	@GetMapping("/counter/decrement")
 	public Customer decrement(String id) {
 		Optional<Customer> customer = repository.findById(id);
 		if (customer.isPresent()) {
